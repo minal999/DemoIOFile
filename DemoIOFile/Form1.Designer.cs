dbe6a7392278f271c6249a0252b3d308687ae57a
minal999 <minughadge555@gmail.com>
2022-10-31 17:57:59 +0530

    Add project files.

---
+++ DemoIOFile/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DemoIOFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnDirectory = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(57, 199);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(234, 199);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnDirectory
+            // 
+            this.btnDirectory.Location = new System.Drawing.Point(57, 278);
+            this.btnDirectory.Name = "btnDirectory";
+            this.btnDirectory.Size = new System.Drawing.Size(75, 51);
+            this.btnDirectory.TabIndex = 2;
+            this.btnDirectory.Text = "Create Directory";
+            this.btnDirectory.UseVisualStyleBackColor = true;
+            this.btnDirectory.Click += new System.EventHandler(this.btnDirectory_Click);
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(234, 278);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 51);
+            this.btnFile.TabIndex = 3;
+            this.btnFile.Text = "Create File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(54, 51);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(46, 13);
+            this.lblID.TabIndex = 4;
+            this.lblID.Text = "Enter ID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(54, 93);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Enter Name";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(54, 134);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(76, 13);
+            this.lblLocation.TabIndex = 6;
+            this.lblLocation.Text = "Enter Location";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(194, 44);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(149, 20);
+            this.txtID.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(194, 90);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(149, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(194, 131);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(149, 20);
+            this.txtLocation.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.btnDirectory);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Department Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnDirectory;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLocation;
+    }
+}
---
+++ DemoIOFile/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoIOFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"D:\DemoIOFile\Test.txt", FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                txtID.Text = br.ReadInt32().ToString();
+                txtName.Text = br.ReadString();
+                txtLocation.Text = br.ReadString();
+                br.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"D:\DemoIOFile\Test.txt", FileMode.Create, FileAccess.Write);
+                BinaryWriter br = new BinaryWriter(fs);
+                br.Write(Convert.ToInt32(txtID.Text));
+                br.Write(txtName.Text);
+                br.Write(txtLocation.Text);
+                br.Close();
+                fs.Close();
+                MessageBox.Show("Data added to file");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDirectory_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\DemoIOFile";
+            if (Directory.Exists(path))
+            {
+                MessageBox.Show("Directory is already exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+                MessageBox.Show("Directory Created");
+            }
+
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\DemoIOFile\Test.txt";
+            if (File.Exists(path))
+            {
+                MessageBox.Show("File is already exists");
+            }
+            else
+            {
+                File.Create(path);
+                MessageBox.Show("File Created");
+            }
+        }
+
+     
+    }
+}
